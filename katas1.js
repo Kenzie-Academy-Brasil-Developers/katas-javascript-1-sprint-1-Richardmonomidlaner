0ad8515c69bfd8c37ca5fd392c6684de8b62d541
unknown <richardbarbosasilva44@gmail.com>
2021-08-12 17:49:17 -0400

    entrega atividade 61

--- katas1.js
+++ katas1.js
@@ -1,86 +1,91 @@
 function oneThroughTwenty() {
-
-  let x=[];
-   for (let i = 1; i <= 20; i++){}
-return x;
+  let x = [];
+  for (let i = 1; i <= 20; i++) {
+    x += i + ", ";
+  }
+  return x;
 }
-console.log (oneThroughTwenty());
+console.log(oneThroughTwenty());
 
 function evensToTwenty() {
-    let even= [];
-   for (let i=0; i<=20; i++){
-   if (i%2===0); } 
-   return even;
+  let even = [];
+  for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) even += i + ", ";
+  }
+  return even;
 }
 console.log(evensToTwenty());
 
-
 function oddsToTwenty() {
-    let odd=[];
-        for (let i=0; i < 20; i++) {
-    if(i%2===1);
-    odd += i; } 
-    return odd;    
+  let odd = [];
+  for (let i = 20; i >= 0; i--) {
+    if (i % 2 === 1) odd += i + ", ";
+  }
+  return odd;
 }
 console.log(oddsToTwenty());
 
-function multiplesOfFivetoOnehundred() {
-  let num1 = [0]; 
-     for (let i = num1; i <= 20; i += num1 * 5){
-     num1 += i;}
-     return num1;
+function multiplesof5to100() {
+  let multiple = [];
+  for (let i = 0; i <= 100; i++) {
+    if (i % 5 === 0) multiple += i + ", ";
+  }
+  return multiple;
 }
-console.log(multiplesOfFivetoOnehundred());
+console.log(multiplesof5to100());
 
-
-function squareNumberstoOnehundred() {
-  let num1 =[];
-  for (let i = 0; i <= 10; i ++){
-    if (i * i <= 100);
-    num1 += i;
-} return num1
+function squareto100() {
+  let square = [];
+  for (let i = 0; i <= 10; i++) {
+    square += Math.pow(i, 2) + ", ";
+  }
+  return square;
 }
-console.log(squareNumberstoOnehundred());
-
+console.log(squareto100());
 
 function countingBackwards() {
-  let x=[];
-  for (let i = 20; i <= 20; i--){}
-   return x; 
+  let x = [];
+  for (let i = 20; i >= 0; i--) {
+    x += i + ", ";
+  }
+  return x;
 }
 console.log(countingBackwards());
 
-
 function evenNumbersBackwardstoTwenty() {
-  let even= [];
-  for (let i = 20; i <= 20; i--){
-  if (i%2===0);
-  } return even;   
+  let even = [];
+  for (let i = 20; i >= 0; i--) {
+    if (i % 2 === 0) {
+      even += i + ", ";
+    }
+  }
+  return even;
 }
 console.log(evenNumbersBackwardstoTwenty());
 
-
-
-function oddNumbersBackwardstoTwenty() {
-  let odd=[];
-  for (let i=20; i <= 20; i--) {
-if(i%2===1);
-  } return odd;    
-}
-console.log(oddNumbersBackwardstoTwenty());
-
-
-function multiplesOfFiveBackwardstoOnehundred() {
-  let num1 = [] 
-     for (let i = 100; i <= 100; i -= num1 - 5){
-     } return multiples;
-}
-console.log(multiplesOfFiveBackwardstoOnehundred());
-
-function squareNumbersBackwards() {
-  let num =[];
-  for (let i = 100; i <= 100; i --) {
-    if (Math.sqrt(i) <=100);
-    return num;}
+function BackwardsoddsToTwenty() {
+  let odd = [];
+  for (let i = 20; i >= 0; i--) {
+    if (i % 2 === 1) odd += i + ", ";
   }
-  console.log(squareNumbersBackwards());
+  return odd;
+}
+console.log(BackwardsoddsToTwenty());
+
+function Backwardsmultiplesof5to100() {
+  let multiple = [];
+  for (let i = 100; i >= 0; i--) {
+    if (i % 5 === 0) multiple += i + ", ";
+  }
+  return multiple;
+}
+console.log(Backwardsmultiplesof5to100());
+
+function Backwardssquareto100() {
+  let square = [];
+  for (let i = 10; i >= 0; i--) {
+    square += Math.pow(i, 2) + ", ";
+  }
+  return square;
+}
+console.log(Backwardssquareto100());
